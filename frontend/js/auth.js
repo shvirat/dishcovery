61833709fe76b99b29249fa99639f7213ce64e7f
Virat Sharma <emvirat@gmail.com>
2025-12-31 13:42:16 +0530

    Added API into frontend

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -41,7 +41,7 @@ const API_BASE =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "";
+    : "https://dishcovery-api.vercel.app";
 
 
 // Toast notification function
